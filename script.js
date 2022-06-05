8bbd2c27a6e7de90b94d82dc14434e3e93f88d50
PBennato <pedrobennato@hotmail.com>
2022-06-05 17:58:11 -0300

    first

---
+++ script.js
@@ -0,0 +1,29 @@
+let contador=0;
+let total=0;
+let division=0;
+
+
+function agregar(){
+    let nombre=$("#usuario").val();
+    let dinero=$("#gasto").val();
+    contador +=1;
+
+    dividir( dinero );
+
+    let displayName = `<h5> ${nombre}: </h5>`
+    let displayMoney = `<h5> ${dinero} </h5>`
+
+    $("#nameToShow").append(displayName);
+    $("#moneyToShow").append(displayMoney);
+
+}
+
+function dividir(dinero){
+    total += parseInt(dinero);
+    
+    $("#total").text("$"+ total);
+
+    division = total / contador;
+
+    $("#cadaUno").text("$"+ division);   
+}
